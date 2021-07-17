74d70638f20c6bd807d00c098b50778654f1db00
Ygor Nascimento <ygor.nascimento@outlook.com>
2021-07-17 12:10:35 -0300

    Add a function to improve ViewModel icon property

--- Cloudy/Extensions/UIIMage+Extension.swift
+++ Cloudy/Extensions/UIIMage+Extension.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2021 Cocoacasts. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+    
+    class func imageForIcon(with name: String) -> UIImage? {
+        
+        switch name {
+        case "clear-day", "clear-night", "rain", "snow", "sleet":
+            return UIImage(named: name)
+        case "wind", "cloudy", "partly-cloudy-day", "partly-cloud-night":
+            return UIImage(named: "cloudy")
+        default:
+            return UIImage(named: "clear-day")
+        }
+    }
+}
+
+
