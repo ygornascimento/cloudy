ecfbe9e24e60e7aec4e7e99469afd8c0858bcd91
Ygor Nascimento <ygor.nascimento@outlook.com>
2021-07-17 12:11:27 -0300

    Added function to improve time property on DayViewModel

--- Cloudy/Types/TimeNotation.swift
+++ Cloudy/Types/TimeNotation.swift
@@ -13,4 +13,13 @@ enum TimeNotation: Int {
     case twelveHour
     case twentyFourHour
     
+    var timeFormat: String {
+        switch self {
+        case .twelveHour:
+            return "hh:mm a"
+        case .twentyFourHour:
+            return "HH:mm"
+        }
+    }
+    
 }
